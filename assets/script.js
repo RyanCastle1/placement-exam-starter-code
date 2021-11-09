74bb30f7f44305b4c3b4531a12a0b6acb4ccb53a
RyanCastle1 <92881670+RyanCastle1@users.noreply.github.com>
2021-11-09 10:28:17 -0600

    finished and working code

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 
-let yourName= 'Ryan Castle',
-const; gingerBreadTotal = document.getElementById('qty-gb');
+let yourName= 'Ryan Castle';
+const gingerBreadTotal = document.getElementById('qty-gb');
 const chocChipTotal = document.getElementById('qty-cc');
-const sugarCookieTotal = document.getElementById('qty-sugar');
+const sugarTotal = document.getElementById('qty-sugar');
 const cookieTotal = document.getElementById('qty-total');
 
 let gb = 0;
@@ -12,53 +12,60 @@ let sugar = 0;
 let Totals = (gb+cc+sugar);
 
 
-document.getElementById('credit').Content= 'created by $(yourName)';
+document.getElementById('credit').textContent= 'created by $(yourName)';
 document.getElementById('minus-gb').addEventListener('click', function() {
     
-    if ( gb ==0 ) {
+    if ( gb >= 0 ) {
         gb--;
-        gingerBreadTotal.Content = gb;
-        Totals = (gb+cc+sugar);
-        cookieTotals.Content = parseInt(cookieTotals);
+        gingerBreadTotal.textContent = gb;
+        Totals= (gb+cc+sugar);
+        cookieTotal.textContent = parseInt(cookieTotal);
+        console.log(gb);
     }
 });
 document.getElementById('add-gb').addEventListener('click', function() {
-if (gb==0) {
+if (gb >= 0) {
     gb++;
-    gingerBreadTotal.Content = gb;
+    gingerBreadTotal.textContent = gb;
     Totals = (gb+cc+sugar);
-    cookieTotals.Content = parseInt(cookieTotals);} 
+    cookieTotal.textContent = parseInt(cookieTotal);
+    console.log(gb);
+}
 });
 document.getElementById('minus-cc').addEventListener('click', function() {
     
-    if ( cc == 0 ) {
+    if ( cc >= 0 ) {
         cc--;
-        chocChipTotal.Content = cc;
+        chocChipTotal.textContent = cc;
         Totals = (gb+cc+sugar);
-        cookieTotals.Content = parseInt(cookieTotals);
+        cookieTotals.textContent = parseInt(cookieTotal);
+        console.log(cc);
     }
 });
 document.getElementById('add-cc').addEventListener('click', function() {
-    if (cc==0) {
+    if ( cc >= 0) {
     cc++;
     chocChipTotal.Content = cc;
     Totals = (gb+cc+sugar);
-    cookieTotals.Content = parseInt(cookieTotals);
+    cookieTotal.Content = parseInt(cookieTotal);
+    console.log(cc);
     }
 });
 document.getElementById('minus-sugar').addEventListener('click', function() {
-if (sugar == 0 ) {
+if ( sugar >= 0 ) {
         sugar--;
         sugarCookietotal.Content = sugar;
         Totals = (gb+cc+sugar);
-        cookieTotals.text = parseInt(cookieTotals);
-    }
+        cookieTotal.textContent = parseInt(cookieTotal);
+        console.log(sugar);
+}
 });
 document.getElementById('add-sugar').addEventListener('click', function() {
-    if ('sugar' ==0) {
+    if ( sugar >= 0) {
     sugar++;
     sugarCookietotal.Content = sugar;
     Totals = (gb+cc+sugar);
-    cookieTotals.Content = parseInt(cookieTotals);
+    cookieTotal.textContent = parseInt(cookieTotal);
+    console.log(sugar);
     }
 });
